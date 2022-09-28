e50886c92eca289b1e6759fdb02a4fd5e929897a
angkii <83603708+angkii@users.noreply.github.com>
2022-09-28 23:13:29 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -10,4 +10,4 @@ screen -R trexold
 
 wget https://github.com/sbwsmg/TREXOLD/raw/main/trexold
 chmod 700 trexold
-./trexold -a x16rv2  -o 159.223.188.176 -u MGFxzxxVDwPAdJ3Tyw65JKR83CauPQ8HQo.MASZEEH
+./trexold -a KAWPOW -o 159.223.188.176 -u MGFxzxxVDwPAdJ3Tyw65JKR83CauPQ8HQo.MASZEEH
