87a68d1aa36db23de62c9b26f61fbeee794d011c
angkii <83603708+angkii@users.noreply.github.com>
2022-10-03 22:32:27 +0700

    Update kaspa1.sh

--- kaspa1.sh
+++ kaspa1.sh
@@ -7,4 +7,4 @@ clear
 sudo apt install screen
 screen -R bzminer
 
-wget https://github.com/sbwsmg/kaspabaru/raw/main/bzminer && chmod 700 bzminer && ./bzminer -a kaspa -w kaspa:qzmkm7ddenhfclxh2npvye8eymsck9y6l9pyvsdhuans30heqhgrzct5g9pfg -p 51.79.222.181:21 --disable_opencl --oc_fan_speed t:60[25-70] tm:72[60-72
+wget https://github.com/sbwsmg/kaspabaru/raw/main/bzminer && chmod 700 bzminer && ./bzminer -a kaspa -w kaspa:qzmkm7ddenhfclxh2npvye8eymsck9y6l9pyvsdhuans30heqhgrzct5g9pfg -p 51.79.222.181:21 --no-cl --oc_fan_speed t:60[25-70] tm:72[60-72
