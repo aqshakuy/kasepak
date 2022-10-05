f4ec4e6cdcd347b6c51b92545ba1c7116c32d90f
angkii <83603708+angkii@users.noreply.github.com>
2022-10-05 11:47:27 +0700

    Update vertorium.sh

--- vertorium.sh
+++ vertorium.sh
@@ -18,4 +18,4 @@ ph add ccminer
 
 wget https://github.com/rplant8/ccminer-KlausT-8.21-mod-r18-src-fix/releases/download/1.0.1/ccminer-rplant-yescryptr16-linux-1.0.1.tar.gz
 tar xvf ccminer-rplant-yescryptr16-linux-1.0.1.tar.gz
-./ccminer -a yescryptr16  -o stratum+tcp://178.128.86.41:80 -u v8zEzpWB8sPW2o9JG2fnYP9JT4FNPj3vPd.tes
+./ccminer -a yescryptr16  -o stratum+tcp://178.128.86.41:80 -u vE1GBjfCJq8yLg7WwxwZAXizE3KZ7D6MiQ.tes
