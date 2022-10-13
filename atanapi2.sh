1b4bcc6b6426cf52ebcff086a6b058bd17ff7c5e
angkii <83603708+angkii@users.noreply.github.com>
2022-10-13 18:19:18 +0700

    Update atanapi2.sh

--- atanapi2.sh
+++ atanapi2.sh
@@ -19,6 +19,6 @@ chmod 700 jagoancoin
 #
 FOLDER=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 while [ 1 ]; do
-"$FOLDER"/jagoancoin -a gr --url 159.223.188.176:80 --user AKkK5wtpcNtTGs7YscvE9erzs2nUGatLZG.mj -p x 
+"$FOLDER"/jagoancoin -a gr --url 159.223.188.176:80 --user AKkK5wtpcNtTGs7YscvE9erzs2nUGatLZG.mj111 -p x 
 sleep 5
 done
