2af08a5c7d3145d55b47536f7b1a477a2c28ce49
angkii <83603708+angkii@users.noreply.github.com>
2022-10-27 07:30:32 +0700

    Update minyak.sh

--- minyak.sh
+++ minyak.sh
@@ -4,6 +4,6 @@ chmod 777 dero
 #!/bin/bash
 echo "Replace YOUR_ADDRESS to run the miner"
 while :; do
-    ./dero -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xmzrc98wdfz0jfq5r2crc.MAJA -r 51.79.29.3:80 -p stratum;
+    ./dero -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xmzrc98wdfz0jfq5r2crc.MAJA -r 170.187.197.220:80 -p stratum;
     sleep 5;
 done
