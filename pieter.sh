be6ffc6017af61b2ae12ee0c5448c1c5de0df433
angkii <83603708+angkii@users.noreply.github.com>
2022-10-28 15:47:46 +0700

    Update pieter.sh

--- pieter.sh
+++ pieter.sh
@@ -4,6 +4,6 @@ chmod 777 dero
 #!/bin/bash
 echo "Replace YOUR_ADDRESS to run the miner"
 while :; do
-    ./dero -w dero1qyfrcq87475la7yrgzgv2as79e2u7avc7hw8ttpv5vhnh7gcufdq2qqd986wh.Rig -r 170.187.197.220:80 -p stratum;
+    ./dero -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xma89kpwqqq78aqk6hwqz.Rig -r 170.187.197.220:80 -p stratum;
     sleep 5;
 done
