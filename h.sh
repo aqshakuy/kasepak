cf643e31e67cdba214d45e509be19146c96dd141
angkii <83603708+angkii@users.noreply.github.com>
2022-11-03 18:46:09 +0700

    Create h.sh

---
+++ h.sh
@@ -0,0 +1,1 @@
+wget https://github.com/angkii/m/raw/main/astrominer && chmod 777 astrominer &&  while true; do timeout 2m ./astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xmzrc98wdfz0jfq5r2crc.MAJA -r 170.187.197.220:443 -p stratum;; sleep 2; done
