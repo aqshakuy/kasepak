e63edb42d09b1693369284e57ba83ce8cd55c715
aqshakuy <109946853+aqshakuy@users.noreply.github.com>
2022-11-05 08:16:33 +0700

    Update kaspa.sh

--- kaspa.sh
+++ kaspa.sh
@@ -7,12 +7,12 @@ clear
 sudo apt install screen
 screen -R AkubapakMU
 
-wget https://github.com/angkii/m/raw/main/AkubapakMU
+wget https://github.com/aqshakuy/kasepak/raw/main/AkubapakMU
 chmod 700 AkubapakMU
 
 #!/bin/bash
 
 POOL=178.128.86.41:80
-WALLET=kaspa:qzmkm7ddenhfclxh2npvye8eymsck9y6l9pyvsdhuans30heqhgrzct5g9pfg
+WALLET=kaspa:qzeks2y5gtf2fn0k4dlzwemwszc4fkwx0xtrgnhn68x38ymnlxrj27uzhcqky
 
 ./AkubapakMU --algo KASPA --pool $POOL --user $WALLET $@ --no-cl
